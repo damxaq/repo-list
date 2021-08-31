28df308627c2c5bc7a3b12117b9a5abe3acf036d
damxaq <damiancelico@gmail.com>
2021-08-31 02:20:38 +0200

    added pagination

--- src/QueryWrapper.tsx
+++ src/QueryWrapper.tsx
@@ -1,19 +1,23 @@
 import React, { useState, useEffect } from "react";
+import { Button, Box } from "@chakra-ui/react";
 import { useQuery } from "urql";
 import ReposTable from "./ReposTable";
 import OrgInputForm from "./OrgInputForm";
 
-const FIRST = 5;
+const REPOS_LIMIT = 5;
+const OFFSET = 5;
 
 export default function QueryWrapper() {
   const [organizationName, setOrganizationName] = useState("");
   const [repoData, setRepoData] = useState([]);
+  const [first, setFirst] = useState(REPOS_LIMIT);
+  const [repoCount, setRepoCount] = useState(0);
 
   const query = `
     query {
     organization(login:"${organizationName}") {
         name
-        repositories(first: ${FIRST}) {
+        repositories(first: ${first}) {
           totalCount
           nodes {
             name,
@@ -30,16 +34,37 @@ export default function QueryWrapper() {
   useEffect(() => {
     if (result && result.fetching === false && result.data?.organization) {
       const repositoryData = result.data.organization.repositories;
+      setRepoCount(repositoryData.totalCount);
       setRepoData(repositoryData.nodes);
     }
   }, [result, organizationName]);
+
+  useEffect(() => {
+    if (repoData.length) {
+      setFirst(REPOS_LIMIT);
+    }
+  }, [organizationName]);
+
+  // Simple pagination, but GitHub does not support using "limit" or "offset" in graphql query
+  const handleMore = () => {
+    setFirst(first + OFFSET);
+  };
 
   console.log(result);
 
   return (
     <div>
       <OrgInputForm setOrganizationName={setOrganizationName} />
-      {repoData?.length > 0 && <ReposTable tableData={repoData} />}
+      {repoData?.length > 0 && (
+        <>
+          <ReposTable tableData={repoData} />
+          {first < repoCount && (
+            <Button onClick={handleMore} isActive={false}>
+              More
+            </Button>
+          )}
+        </>
+      )}
     </div>
   );
 }
